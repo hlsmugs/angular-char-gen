ab1e0d90afbc7308d566226d35f1f651b066b0fa
Mugs <jjmiguel.diaz@gmail.com>
2025-01-31 10:24:41 -0500

    app ini

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AppComponent } from './app.component';
+import { app } from '../../server';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        component: HomeComponent,
+      },
+      {
+        path: 'home',
+        redirectTo: '',
+      }
+
+];
