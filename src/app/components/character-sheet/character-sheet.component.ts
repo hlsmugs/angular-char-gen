bafc1e50f8c456a2eb702a7d10770d992bdd9415
Miguel Diaz <jjmiguel.diaz@gmail.com>
2025-02-03 17:46:47 -0500

    default json files for dropdowns; character sheet form structure;

--- src/app/components/character-sheet/character-sheet.component.ts
+++ src/app/components/character-sheet/character-sheet.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import defaultClasses from '../../../../public/json/default-classes.json';
+import defaultSpecies from '../../../../public/json/default-species.json';
+import defaultBackgrounds from '../../../../public/json/default-backgrounds.json';
+
+import { CharacterClass } from '../../models/character-class.model';
 
 @Component({
   selector: 'app-character-sheet',
   standalone: true,
   imports: [],
   templateUrl: './character-sheet.component.html',
-  styleUrl: './character-sheet.component.scss'
+  styleUrl: './character-sheet.component.scss',
 })
 export class CharacterSheetComponent {
-
+  public characterClasses: {
+    characterClassName: string;
+    hitDiceSize: number;
+    isSpellcaster: boolean;
+    spellcastingAbility: string;
+  }[] = defaultClasses;
+  public characterSpecies: {
+    speciesName: string;
+  }[] = defaultSpecies;
+  public characterBackground: {
+    backgroundName: string;
+    backgroundDescription: string;
+  }[] = defaultBackgrounds;
 }
