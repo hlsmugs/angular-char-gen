2de18d4cb8e275585b5f73bc6cfd8f9fb25c39ef
Miguel Diaz <jjmiguel.diaz@gmail.com>
2025-02-15 14:02:21 -0500

    firebase hosting

---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,9 @@
+const { , validateArgs } = require('firebase/data-connect');
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'angular-char-gen',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
