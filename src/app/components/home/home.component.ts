5a5d28bed5e8b5ae362987c4e009905142ae46ba
Miguel Diaz <jjmiguel.diaz@gmail.com>
2025-02-22 15:42:09 -0500

    home page link boxes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import linkboxJson from '../../../../public/json/link-box-content.json';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +12,8 @@ import { RouterModule } from '@angular/router';
 })
 export class HomeComponent {
   public title: string = 'Home';
+  public linkboxes: any[] = linkboxJson;
+
   constructor(private readonly titleService: Title) {
     this.titleService.setTitle(this.title);
   }
