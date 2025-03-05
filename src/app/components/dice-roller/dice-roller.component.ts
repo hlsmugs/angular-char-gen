da640aeb2e2a2661f844dd4e604c78779c92ca5b
Miguel Diaz <jjmiguel.diaz@gmail.com>
2025-03-05 16:27:52 -0500

    dice roller updated

--- src/app/components/dice-roller/dice-roller.component.ts
+++ src/app/components/dice-roller/dice-roller.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { DiceRollerService } from '../../services/dice-roller.service';
+import { NumberSymbol } from '@angular/common';
+import { error } from 'console';
 
 @Component({
   selector: 'app-dice-roller',
@@ -13,12 +15,14 @@ export class DiceRollerComponent {
   rollSum?: any[];
   numberOfDice: number;
   diceSize: number;
+  maxNumberOfDice: number;
 
   constructor(private diceRollerService: DiceRollerService) {
     this.numberOfDice = 0;
     this.diceSize = 0;
     this.rollResults = [];
     this.rollSum = [];
+    this.maxNumberOfDice = 10000;
   }
 
   setNumberOfDice(evt: any) {
@@ -29,13 +33,29 @@ export class DiceRollerComponent {
     this.diceSize = evt.target.value;
   }
 
+  clearResults() {
+    this.rollResults = [];
+  }
+
   rollDice() {
-    let res: number[] = this.diceRollerService.multiRoll(
-      this.numberOfDice,
-      this.diceSize
-    );
-    this.rollResults?.push(res);
-    this.rollSum?.push(this.diceRollerService.getSumOfRolls(res));
-    console.log(this.rollSum);
+    const isValid =
+      this.numberOfDice > 0 &&
+      this.numberOfDice < this.maxNumberOfDice &&
+      this.diceSize > 0;
+    if (isValid) {
+      let res: number[] = this.diceRollerService.multiRoll(
+        this.numberOfDice,
+        this.diceSize
+      );
+      this.rollResults?.push(res);
+      this.rollSum?.push(this.diceRollerService.getSumOfRolls(res));
+    } else {
+      console.error('Invalid entry');
+    }
+    // if (this.numberOfDice > this.maxNumberOfDice) {
+    //   console.error('Too many dice');
+    // } else {
+    //   console.error('No Dice');
+    // }
   }
 }
